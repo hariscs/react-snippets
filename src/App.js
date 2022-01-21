a89fd49e97181600e531705e19450cda4b293958
Haris Shah <harisshah385@gmail.com>
2022-01-21 18:18:22 +0500

    search data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+import data from './data';
+
 function App() {
-	return <h1>React Snippets</h1>;
+	const [filteredData, setFilteredData] = useState(data);
+
+	const search = (e) => {
+		setFilteredData(
+			data.filter((item) => item.title.toLowerCase().includes(e.target.value))
+		);
+	};
+
+	return (
+		<div className='app'>
+			<h1>React Search</h1>
+			<input type='text' placeholder='Search the data' onKeyUp={search} />
+			<ul>
+				{filteredData.map((user) => (
+					<li key={user.id}>{user.title}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+const data = [
+	{ id: 2, title: 'quis ilis et qui', completed: false },
+	{ id: 3, title: 'fugiat veniam minus', completed: false },
+	{ id: 4, title: 'et porro tempora', completed: true },
+	{ id: 5, title: 'laboriosam mont illum', completed: false },
+	{ id: 6, title: 'qui ullam ratione qunis', completed: false },
+	{ id: 7, title: 'illo expedita consequatur quia in', completed: false },
+	{ id: 8, title: 'quo adipisci enim quam ut ab', completed: true },
+	{ id: 9, title: 'molestiae perspiciatis ipsa', completed: false },
+];
+
+export default data;
